f591686fd231d5cee566cbf0b265b26c26152aaf
十七 <10858187+shiqi547git@user.noreply.gitee.com>
2023-03-05 16:18:37 +0800

    init:初始化

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="name">welcome to shiqi's page</h1>
+</template>
+<script setup></script>
+<style>
+.name {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueSetupExtend from "vite-plugin-vue-setup-extend";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VueSetupExtend()],
+});
